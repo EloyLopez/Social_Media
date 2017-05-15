3bb4fb8085dd5b7e20540b635ee5eaa1159f1d12
EloyLopez <towel.winger@gmail.com>
2017-05-15 23:10:18 +0200

    add usuario rdy

--- SocialMedia/app/src/main/java/edu/lasalle/ppro2/socialmedia/activities/AddUsuarioActivity.java
+++ SocialMedia/app/src/main/java/edu/lasalle/ppro2/socialmedia/activities/AddUsuarioActivity.java
@@ -2,6 +2,7 @@ package edu.lasalle.ppro2.socialmedia.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import edu.lasalle.ppro2.socialmedia.R;
 
@@ -11,5 +12,10 @@ public class AddUsuarioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_usuario);
+        getSupportActionBar().hide();
+
+    }
+    public void back_toolBar(View view){
+        onBackPressed();
     }
 }
--- SocialMedia/app/src/main/java/edu/lasalle/ppro2/socialmedia/activities/LoginActivity.java
+++ SocialMedia/app/src/main/java/edu/lasalle/ppro2/socialmedia/activities/LoginActivity.java
@@ -1,27 +1,12 @@
 package edu.lasalle.ppro2.socialmedia.activities;
 
-import android.content.Context;
+
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
-import java.io.Writer;
 
 import edu.lasalle.ppro2.socialmedia.R;
 
